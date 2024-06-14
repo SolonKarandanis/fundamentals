53aff00de00354348f27d21131ce37c3c1448ad6
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-06-14 12:31:58 +0300

    First commit

---
+++ src/app.module.ts
@@ -0,0 +1,21 @@
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { SongsModule } from './songs/songs.module';
+import { LoggerMiddleware } from './common/middleware/logger.middleware';
+import { SongsController } from './songs/songs.controller';
+
+@Module({
+  imports: [
+    SongsModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule implements NestModule{
+
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes(SongsController);
+  }
+
+}
