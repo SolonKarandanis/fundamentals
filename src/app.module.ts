8ed1ed38ffd418f29787ba760ed6a10e14c094d8
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-06-21 19:46:22 +0300

    Created connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,15 +4,29 @@ import { AppService } from './app.service';
 import { SongsModule } from './songs/songs.module';
 import { LoggerMiddleware } from './common/middleware/logger.middleware';
 import { SongsController } from './songs/songs.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
 
 @Module({
   imports: [
     SongsModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'fundamentals',
+      password: 'fundamentals',
+      database: 'fundamentals',
+      entities: [],
+      synchronize: true,
+    })
   ],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule implements NestModule{
+
+  constructor(private dataSource: DataSource){}
 
   configure(consumer: MiddlewareConsumer) {
     consumer.apply(LoggerMiddleware).forRoutes(SongsController);
