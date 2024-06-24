5991c5527e81cba4dda7fe0356deb8e00857589c
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-06-24 08:46:14 +0300

    Implementing song service

--- src/songs/songs.service.ts
+++ src/songs/songs.service.ts
@@ -1,13 +1,50 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { DeleteResult, Repository, UpdateResult } from 'typeorm';
+import { Song } from './song.entity';
+import { CreateSongDTO } from './dto/create-song-dto';
+import { UpdateSongDto } from './dto/update-song-dto';
 
 @Injectable()
 export class SongsService {
-    
-    create(song){
 
+    constructor(
+        @InjectRepository(Song)
+        private songsRepository: Repository<Song>,
+    ){}
+    
+    async create(songDTO: CreateSongDTO): Promise<Song> {
+        const song = new Song();
+        song.title = songDTO.title;
+        // song.artists = songDTO.artists;
+        song.duration = songDTO.duration;
+        // song.lyrics = songDTO.lyrics;
+        song.releasedDate = songDTO.releasedDate;
+    
+        console.log(songDTO.artists);
+    
+        // find all the artits on the based on ids
+        // const artists = await this.artistsRepository.findByIds(songDTO.artists);
+        // console.log(artists);
+        //set the relation with artist and songs
+        // song.artists = artists;
+    
+        return this.songsRepository.save(song);
     }
   
-    findAll(){
-      
+    findAll(): Promise<Song[]> {
+        return this.songsRepository.find();
+    }
+    
+    findOne(id: number): Promise<Song> {
+        return this.songsRepository.findOneBy({ id });
+    }
+    
+    remove(id: number): Promise<DeleteResult> {
+        return this.songsRepository.delete(id);
+    }
+    
+    update(id: number, recordToUpdate: UpdateSongDto): Promise<UpdateResult> {
+        return this.songsRepository.update(id, recordToUpdate);
     }
 }
