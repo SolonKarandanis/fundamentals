686fd86d4319404c34208879f7e60661140c93fd
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-06-25 08:59:47 +0300

    Implementing auth controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -6,10 +6,16 @@ import {
   Param,
   Patch,
   Post,
+  UseGuards,
+  Request,
 } from '@nestjs/common';
 
 import { AuthService } from './auth.service';
 import { UsersService } from 'src/users/users.service';
+import { AuthGuard } from '@nestjs/passport';
+import { JwtAuthGuard } from './jwt-guard';
+import { UpdateResult } from 'typeorm';
+import { ValidateTokenDTO } from './dto/validate-token.dto';
 
 @Controller('auth')
 // @ApiTags('auth')
@@ -20,5 +26,43 @@ export class AuthController {
   ) {}
 
 
+  @Post('validate-2fa')
+  @UseGuards(JwtAuthGuard)
+  validate2FA(
+    @Request()
+    req,
+    @Body()
+    ValidateTokenDTO: ValidateTokenDTO,
+  ): Promise<{ verified: boolean }> {
+    return this.authService.validate2FAToken(
+      req.user.userId,
+      ValidateTokenDTO.token,
+    );
+  }
+  @Get('disable-2fa')
+  @UseGuards(JwtAuthGuard)
+  disable2FA(
+    @Request()
+    req,
+  ): Promise<UpdateResult> {
+    return this.authService.disable2FA(req.user.userId);
+  }
+  
+  @Get('profile')
+  @UseGuards(AuthGuard('bearer'))
+  getProfile(
+    @Request() req,
+  ) {
+    delete req.user.password;
+    return {
+      msg: 'authenticated with api key',
+      user: req.user,
+    };
+  }
+
+  @Get('test')
+  testEnvVariable() {
+    return this.authService.getEnvVariable();
+  }
 
 }
