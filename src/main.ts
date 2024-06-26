2c14388d00ae8201fc5d2cb5533f8760f1dc9ca5
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-06-26 09:30:30 +0300

    Hot reloading

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,16 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import "reflect-metadata"
 
+declare const module: any;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 }
 bootstrap();
