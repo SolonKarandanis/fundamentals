b7d173aa14a9919bcab053e5b6e4f021fb4f5d44
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-06-26 08:52:17 +0300

    Created playlist functionality

--- src/playlists/playlist.controller.ts
+++ src/playlists/playlist.controller.ts
@@ -8,10 +8,19 @@ import {
   Post,
 } from '@nestjs/common';
 import { PlaylistService } from './playlist.service';
+import { CreatePlayListDto } from './dto/create-playlist.dto';
+import { Playlist } from './playlist.entity';
 
 @Controller('playlists')
 export class PlaylistController {
   constructor(private readonly playlistService: PlaylistService) {}
 
- 
+  @Post()
+  create(
+    @Body()
+    playlistDTO: CreatePlayListDto,
+  ): Promise<Playlist> {
+    return this.playlistService.create(playlistDTO);
+  }
+
 }
