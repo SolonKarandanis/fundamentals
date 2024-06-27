563fd420cc3886625114395df2d0745303f20b7a
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-06-27 08:17:01 +0300

    Swagger config

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,34 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import "reflect-metadata"
 import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder() //1
+    .setTitle('Spotify Clone')
+    .setDescription('The Spotify Clone Api documentation')
+    .setVersion('1.0')
+    .addBearerAuth(
+      // Enable Bearer Auth here
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        name: 'JWT',
+        description: 'Enter JWT token',
+        in: 'header',
+      },
+      'JWT-auth', // We will use this Bearer Auth with JWT-auth name on the controller function
+    )
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config); //2
+  SwaggerModule.setup('api', app, document); //3
   
   const configService = app.get(ConfigService);
   await app.listen(configService.get<number>('port'));
