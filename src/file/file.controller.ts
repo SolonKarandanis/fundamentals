bcecc027d35fd83a71fa864d14ecf3119df924ba
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-06-27 09:56:27 +0300

    File uploads

--- src/file/file.controller.ts
+++ src/file/file.controller.ts
@@ -3,14 +3,70 @@ import {
   Controller,
   Delete,
   Get,
+  HttpStatus,
   Param,
+  ParseFilePipeBuilder,
   Patch,
   Post,
+  UploadedFile,
+  UseInterceptors,
 } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
 
 @Controller('files')
 export class FileController {
   constructor() {}
 
 
+  @Post('upload')
+  @UseInterceptors(
+    FileInterceptor('file', {
+      storage: diskStorage({
+        destination: './upload/files',
+        filename: (req, file, cb) => {
+          cb(null, file.originalname);
+        },
+      }),
+    }),
+  )
+  uploadFile(@UploadedFile() file: Express.Multer.File) {
+    console.log(file);
+    return {
+      messge: 'file uploaded successfully!',
+    };
+  }
+
+  // only want to accept png file
+  @Post('upload-png')
+  @UseInterceptors(
+    FileInterceptor('file', {
+      storage: diskStorage({
+        destination: './upload/files',
+        filename: (req, file, cb) => {
+          cb(null, file.originalname);
+        },
+      }),
+    }),
+  )
+  uploadPngFile(
+    @UploadedFile(
+      new ParseFilePipeBuilder()
+        .addFileTypeValidator({
+          fileType: 'png',
+        })
+        // .addMaxSizeValidator({
+        //   maxSize: 70706,
+        // })
+        .build({
+          errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY,
+        }),
+    )
+    file: Express.Multer.File,
+  ) {
+    console.log(file);
+    return {
+      messge: 'file uploaded successfully!',
+    };
+  }
 }
