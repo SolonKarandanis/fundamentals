f6373901450856d498195b7e3f9a5d95114088cd
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-06-27 09:40:44 +0300

    Implementing websocket functionality

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,24 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { dataSourceOptions } from 'db/data-source';
+import { validate } from 'env.validation';
 import { DataSource } from 'typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ArtistModule } from './artists/artist.module';
 import { AuthModule } from './auth/auth.module';
 import { LoggerMiddleware } from './common/middleware/logger.middleware';
+import configuration from './config/configuration';
+import { EventsModule } from './events/events.module';
 import { PlaylistModule } from './playlists/playlist.module';
 import { SongsController } from './songs/songs.controller';
 import { SongsModule } from './songs/songs.module';
 import { UsersModule } from './users/users.module';
-import { ConfigModule } from '@nestjs/config';
-import configuration from './config/configuration';
-import { validate } from 'env.validation';
 
 
 @Module({
-  imports: [PlaylistModule, AuthModule, UsersModule, ArtistModule,
+  imports: [EventsModule, PlaylistModule, AuthModule, UsersModule, ArtistModule,
     SongsModule,
     TypeOrmModule.forRoot(dataSourceOptions),
     ConfigModule.forRoot({
